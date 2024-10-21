a2e912880c17c96cb610a54d928dd7ad1c6f2758
Judith <j.espinalm12@gmail.com>
2024-10-21 17:07:11 -0400

    using printf in this task

---
+++ hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * learning to use prinf instead of puts
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
+}
