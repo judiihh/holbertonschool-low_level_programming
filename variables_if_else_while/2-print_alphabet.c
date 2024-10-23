19d82821b1fedd18c0a0e7cbb2b3230176838cd2
Judith <j.espinalm12@gmail.com>
2024-10-22 21:54:52 -0400

    for loop alphabet in lowercase fix

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,8 @@ int main(void)
 	char letter;
 
 	/* Loop with ASCII values of the lowercase letters (a-z)*/
-	for (letter = 'a'; letter <= 'z'; letter++) {
+	for (letter = 'a'; letter <= 'z'; letter++)
 		putchar(letter); /*this is going to print each letter, one by one*/
-	}
 
 	putchar('\n'); /*this prints a newline after the alphabet code*/
 	return (0);
