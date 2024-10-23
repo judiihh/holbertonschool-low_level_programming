ed2ed42b88542cd21126659d36783a8a89c4da85
Judith <j.espinalm12@gmail.com>
2024-10-23 01:23:48 -0400

    separating numbers with commas and space in between

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,7 +9,8 @@ int main(void)
 
 	for (numbers = 48; numbers <= 57; numbers++)
 		putchar(numbers),
-		(numbers < 57) ? putchar(44) : 0;
+		(numbers < 57) ? putchar(44) : 0,
+		(numbers < 57) ? putchar(32) : 0;
 
 	putchar(10);
 	return (0);
