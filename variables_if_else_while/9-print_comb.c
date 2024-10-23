d1fcc8125996e772bdcbed8718d31d65ed381430
Judith <j.espinalm12@gmail.com>
2024-10-23 01:18:34 -0400

    separating numbers with commas

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point working with putchar and commas
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+		putchar(numbers),
+		(numbers < 57) ? putchar(44) : 0;
+
+	putchar(10);
+	return (0);
+}
