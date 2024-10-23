b1759e877b141bcc48443284f71b7dcd8d81be50
Judith <j.espinalm12@gmail.com>
2024-10-23 17:42:05 -0400

    adding file for this task, compiled both file and print_alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,16 +1,12 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
- * main - Entry point working with main.h and putchar
+ * main - Entry point working with alphabet and main.h
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	char letter;
-
-	for (letter = 'a'; letter <= 'z'; letter++)
-		putchar(letter);
-
-	putchar('\n');
+	print_alphabet();  /* Call the function to print the alphabet */
 	return (0);
 }
