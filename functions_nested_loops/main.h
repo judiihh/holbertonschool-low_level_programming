d0d7755d0113aaa5552bfaef66bb31b4808f4d58
Judith <j.espinalm12@gmail.com>
2024-10-24 00:17:46 -0400

    creating a function that prints 10 times the alphabet and compiling with 3 files

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,6 +3,7 @@
 
 int _putchar(char c);  /* Prototype for _putchar */
 void print_alphabet(void); /*Prototype for void print_alphabet(void) */
+void print_alphabet_x10(void); /*Prototype for void print_alphabet_x10 */
 
 #endif /* MAIN_H */
 
