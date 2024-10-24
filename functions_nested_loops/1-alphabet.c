18493b98d14de7ae2ecda516e56c70227b7bbf5b
Judith <j.espinalm12@gmail.com>
2024-10-23 22:35:55 -0400

    added compilation for 3 files together

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point working with alphabet and main.h
- *Return: Always 0 (Success)
+ * *Return: Always 0 (Success)
  */
 int main(void)
 {
--- functions_nested_loops/print_alphabet.c
+++ functions_nested_loops/print_alphabet.c
@@ -6,10 +6,10 @@
  */
 void print_alphabet(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        putchar(letter);
-    }
-    putchar('\n');
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	
+	_putchar('\n');
 }
