c9e40b433ec71c5ba4095fb31a945ec0d6bd5afb
Judith <j.espinalm12@gmail.com>
2024-10-23 22:12:04 -0400

    fixing both files and compiling

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,9 +2,8 @@
 
 /**
  * main - Entry point working with alphabet and main.h
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	print_alphabet();  /* Call the function to print the alphabet */
