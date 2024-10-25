cb669ce0dab74f835b96f4792ca1beaf9b747593
Judith <j.espinalm12@gmail.com>
2024-10-25 10:11:08 -0400

    function that prints every minute of the day

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -9,5 +9,6 @@ int _isalpha(int c); /* Prototype for int _isalpha(int c) */
 int print_sign(int n); /* Prototype for int print_sign(int n) */
 int _abs(int); /* Prototype for int _abs(int) */
 int print_last_digit(int n); /* Prototype for print_last_digit(int) */
+void jack_bauer(void); /* Prototype for void jack_bauer(void) */
 
 #endif /* MAIN_H */
