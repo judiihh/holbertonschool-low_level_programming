abcc2fd9ee0958396e82b0869f68916039d92ec5
Judith <j.espinalm12@gmail.com>
2024-10-24 22:16:18 -0400

    adding a file with a function that checks for lowercase characters fix

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,6 +4,6 @@
 int _putchar(char c);  /* Prototype for _putchar */
 void print_alphabet(void); /*Prototype for void print_alphabet(void) */
 void print_alphabet_x10(void); /*Prototype for void print_alphabet_x10 */
+int _islower(int c); /* Prototype for int _islower(int c) */
 
 #endif /* MAIN_H */
-
