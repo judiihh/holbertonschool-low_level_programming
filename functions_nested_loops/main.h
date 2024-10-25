b647a4a87bdf6471985ad150e02dd3fbd0222b19
Judith <j.espinalm12@gmail.com>
2024-10-25 17:33:28 -0400

    function that prints all natural numbers from n to 98

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,5 +12,6 @@ int print_last_digit(int n); /* Prototype for print_last_digit(int) */
 void jack_bauer(void); /* Prototype for void jack_bauer(void) */
 void times_table(void); /* Prototype for void times_table(void) */
 int add(int, int); /* Prototype for int add(int, int) */
+void print_to_98(int n); /*Prototype for void print_to_98(int n) */
 
 #endif /* MAIN_H */
