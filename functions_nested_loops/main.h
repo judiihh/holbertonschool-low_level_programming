22531ddc3c4e3120889d314d314aeac8caec103a
Judith <j.espinalm12@gmail.com>
2024-10-24 22:59:53 -0400

    creating a file with a function that prints the sign of a number

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,5 +6,6 @@ void print_alphabet(void); /*Prototype for void print_alphabet(void) */
 void print_alphabet_x10(void); /*Prototype for void print_alphabet_x10 */
 int _islower(int c); /* Prototype for int _islower(int c) */
 int _isalpha(int c); /* Prototype for int _isalpha(int c) */
+int print_sign(int n); /* Prototype for int print_sign(int n) */
 
 #endif /* MAIN_H */
