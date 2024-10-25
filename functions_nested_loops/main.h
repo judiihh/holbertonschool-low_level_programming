00104a9273b557d250c3cf201b1313935e916bb3
Judith <j.espinalm12@gmail.com>
2024-10-24 22:27:21 -0400

    creating a file with a function that checks for alphabetic character

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,6 @@ int _putchar(char c);  /* Prototype for _putchar */
 void print_alphabet(void); /*Prototype for void print_alphabet(void) */
 void print_alphabet_x10(void); /*Prototype for void print_alphabet_x10 */
 int _islower(int c); /* Prototype for int _islower(int c) */
+int _isalpha(int c); /* Prototype for int _isalpha(int c) */
 
 #endif /* MAIN_H */
