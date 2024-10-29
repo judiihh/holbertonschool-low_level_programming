e0f639ee50d9cf46f7933c84b1797467a9162d06
Judith <j.espinalm12@gmail.com>
2024-10-28 21:24:54 -0400

    function that prints a square fix

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -11,7 +11,7 @@
  */
 void print_square(int size)
 {
-	int i;
+	int i, j;
 
 	if (size <= 0)
 	{
@@ -21,7 +21,10 @@ void print_square(int size)
 
 	for (i = 0; i < size; i++)
 	{
-		_putchar('#');
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
 	}
-	_putchar('\n');
+		_putchar('\n');
 }
