fe447b5a23dcac2908a85b954564a8d4591751b4
Judith <j.espinalm12@gmail.com>
2024-12-05 19:51:30 -0400

    fixed betty style in lists

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,11 +3,19 @@
 
 #include <stddef.h>
 
+/**
+ * struct list_s - Singly linked list
+ * @str: String - (malloc'ed string)
+ * @len: Length of the string
+ * @next: Points to the next node
+ *
+ * Description: Singly linked list node structure
+ */
 typedef struct list_s
 {
-  char *str;
-  unsigned int len;
-  struct list_s *next;
+char *str;
+unsigned int len;
+struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
