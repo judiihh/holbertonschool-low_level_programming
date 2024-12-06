6ebec85569131832bbeffe77f2a6ba581b5bc6cb
Judith <j.espinalm12@gmail.com>
2024-12-06 13:20:09 -0400

    fixed the task for some checkers

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,7 +10,7 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *new_node;
+	dlistint_t *new_node, *temp;
 
 	if (head == NULL)
 		return (NULL);
@@ -20,13 +20,21 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = *head;
+	new_node->next = NULL;
 
-	if (*head != NULL)
-		(*head)->prev = new_node;
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		*head = new_node;
+		return (new_node);
+	}
 
-	*head = new_node;
+	temp = *head;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = new_node;
+	new_node->prev = temp;
 
 	return (new_node);
 }
