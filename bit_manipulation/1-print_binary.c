47f43a21215239775bc4dc7bcdc49da5b69febfb
Judith <j.espinalm12@gmail.com>
2024-12-07 19:56:22 -0400

    added putchar file and a function that prints the binary represent of a number

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_binary - Prints the binary representation of a number.
+ * @n: The number to be printed in binary.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(n) * 8 - 1);
+	int started = 0;
+
+	while (mask)
+	{
+		if (n & mask)
+		{
+			putchar('1');
+			started = 1;
+		}
+		else if (started)
+		{
+			putchar('0');
+		}
+		mask >>= 1;
+	}
+
+	if (!started)
+		putchar('0');
+}
