69db61cf14f51737f9805bbf7df49e9b5cc21b3f
Judith <j.espinalm12@gmail.com>
2024-12-08 21:06:42 -0400

    program that cp the content of a file to another and deleted extra files

---
+++ file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - Copies the content of a file to another file.
+ * @argc: The number of arguments.
+ * @argv: The arguments array.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to, read_bytes, write_bytes;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+		error_exit(97, "Usage: cp file_from file_to", "");
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		error_exit(98, "Error: Can't read from file", argv[1]);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		close(fd_from);
+		error_exit(99, "Error: Can't write to", argv[2]);
+	}
+
+	while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes != read_bytes)
+		{
+			close(fd_from);
+			close(fd_to);
+			error_exit(99, "Error: Can't write to", argv[2]);
+		}
+	}
+
+	if (read_bytes == -1)
+		error_exit(98, "Error: Can't read from file", argv[1]);
+
+	if (close(fd_from) == -1)
+		error_exit(100, "Error: Can't close fd", argv[1]);
+
+	if (close(fd_to) == -1)
+		error_exit(100, "Error: Can't close fd", argv[2]);
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -6,6 +6,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <string.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * error_exit - Handles error messages and exits with a specific code.
+ * @exit_code: The exit code.
+ * @message: The error message to print.
+ * @file_name: The file name to include in the error message.
+ */
+void error_exit(int exit_code, const char *message, const char *file_name)
+{
+    dprintf(STDERR_FILENO, "%s %s\n", message, file_name);
+    exit(exit_code);
+}
 
 int _putchar(int c);
 ssize_t read_textfile(const char *filename, size_t letters);
